ed3407380cf4d0567482acfe18d794ea185e2538
JamaL <jamalikowow@mail.ru>
2024-10-16 19:07:54 +0300

    fixin error in test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:carcraft/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const CarCraft());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
